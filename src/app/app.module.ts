a824f9d7d55de310d823164b7a286c71f247848e
Magdalena Fornalczyk <Magdalena Fornalczyk>
2017-06-20 18:36:02 +0200

    Added car class, and car details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { CarDetailsComponent } from './car-details/car-details.component';
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarDetailsComponent
 
   ],
   imports: [
